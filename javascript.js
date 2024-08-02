d338ac4bd045dab6c2c3cacb48addd667e8d09e5
dannickvaldo <https://email.ionos.de/appsuite/#>
2024-08-02 12:20:49 +0100

    2eme commite

--- javascript.js
+++ javascript.js
@@ -1,14 +1,13 @@
-var acc = document.getElementsByClassName("accordion");
-var i;
+const accordions = document.querySelectorAll('.accordion');
 
-for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
-      this.classList.toggle("active");
-      var panel = this.nextElementSibling;
-      if (panel.style.maxHeight) {
-        panel.style.maxHeight = null;
-      } else {
-        panel.style.maxHeight = panel.scrollHeight + "px";
-      } 
-    });
-  };
+accordions.forEach(accordion => {
+  accordion.addEventListener('click', () => {
+    const panel = accordion.nextElementSibling;
+    accordion.classList.toggle('active');
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + 'px';
+    }
+  });
+});
